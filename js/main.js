b5a5f9bd66e47bbf14bfb251c03555200bfdd234
Bita Djaghouri <bita@bitadj.com>
2017-05-23 15:39:23 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+function LanguageCtrl($scope) {
+
+  $scope.languages = [
+    {
+      "name": "Ruby on Rails", "codeicon": "iconmoon-ruby-dark"
+    },
+    {
+      "name": "HTML5", "codeicon": "fa fa-html5 icon-large"
+    },
+    {
+      "name": "CSS3", "codeicon": "fa fa-css3 icon-large"
+    },
+    {
+      "name": "jQuery", "codeicon": "iconmoon-jquery"
+    },
+    {
+      "name": "JavaScript", "codeicon": "fa fa-shield icon-large"
+    },
+    {
+      "name": "Angular.js", "codeicon": "iconmoon-angular"
+    },
+    {
+      "name": "PostgreSQL", "codeicon": "iconmoon-postgres-black"
+    },
+    {
+      "name": "Mongo DB", "codeicon": "iconmoon-mongo"
+    },
+    {
+      "name": "Firebase", "codeicon": "iconmoon-firebase"
+    },
+    {
+      "name": "API", "codeicon": "fa fa-code icon-large"
+    },
+    {
+      "name": "Twitter Bootstrap", "codeicon": "iconmoon-boostrap"
+    },
+    {
+      "name": "Test-Driven Development", "codeicon": "fa fa-gears icon-large"
+    }
+  ];
+}
